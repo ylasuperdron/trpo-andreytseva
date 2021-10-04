2e2bf6bbde3fc025a608d7a947a77a84cb1578d3
ylasuperdron <julia.andreiceva@gmail.com>
2021-10-04 21:21:35 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<math.h>
+#include<locale>
+#include <stdlib.h>
+using namespace std;
+int main (void)
+{
+	setlocale(LC_ALL, "Russian");
+nn:
+	float a;
+	float b;
+	float c;
+	char d;
+	cout << " Введите первое число >> ";
+	cin >> a;
+
+	cout << " Введите действие >> ";
+	cin >> d;
+
+	cout << " Введите второе число >> ";
+	cin >> b;
+
+	switch (d)
+	{
+	case '+':
+		c = a + b;
+		break;
+	case '-':
+		c = a - b;
+		break;
+	case '*':
+		c = a * b;
+		break;
+	case '/':
+		c = a / b;
+		break;
+		
+	}
+	cout << " Ответ >> " << c << endl;
+
+	
+	
+
+	return 0;
+
+	
+		 
+}
