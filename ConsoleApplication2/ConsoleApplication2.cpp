42c2be9ac194b4f21eb60f81afeda01de2469547
antiban.mixxd <antiban.mixxd@outlook.com>
2021-10-11 22:38:25 +0700

    add navigation menu

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,56 +1,82 @@
-﻿#include<iostream>
-#include<math.h>
-#include<locale>
-#include <stdlib.h>
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <conio.h>
+
+
 using namespace std;
-int main (void)
+int a, b, res;
+
+
+int sum(int a, int b)
 {
-	setlocale(LC_ALL, "Russian");
-	float a;
-	float b;
-	float c;
-	char d;
-	cout << " Введите первое число >> ";
-	cin >> a;
+    int res = a;
 
-	cout << " Введите действие >> ";
-	cin >> d;
+    for (int i = 1; i <= b; ++i)
+    {
+        ++res;
+    }
 
-	cout << " Введите второе число >> ";
-	cin >> b;
-	if (b == 0)
-	{
-		cout << " На 0 делить нельзя! ";
-		return 0;
-	} 
+    return res;
+}
 
-	switch (d)
-	{
-	case '+':
-		c = a + b;
-		break;
-	case '-':
-		c = a - b;
-		break;
-	case '*':
-		c = a * b;
-		break;
-	case '/':
-		c = a / b;
-		break;
+int min(int a, int b)
+{
+    int res = a;
 
-	default:
-		cout << " Неверное действие ";
-		break;
+    for (int i = 1; i <= b; ++i)
+    {
+        --res;
+    }
 
-	}
-	cout << " Ответ >> " << c << endl;
+    return res;
+}
 
-	
-	
+int umg(int a, int b)
+{
+    int res = a * b;
+    return res;
+}
 
-	return 0;
+int prompt_menu_item()
+{
+    setlocale(LC_CTYPE, "rus");
+    system("cls");
+    cout << " Введите первое число >> ";
+    cin >> a;
+    cout << " Введите второе число >> ";
+    cin >> b;
+    int variant;
+    cout << "Выберите вариант\n" << endl;
+    cout << "1. +\n"
+        << "2. -\n"
+        << "3. *\n"
+        << "5. Выход\n" << endl;
+    cout << ">>> ";
+    cin >> variant;
+    return variant;
+}
 
-	
-		 
+int main(int argc, char* argv[])
+{
+    int variant = prompt_menu_item();
+
+    switch (variant) {
+    case 1: sum(a, b);
+        std::cout << "sum = " << sum(a, b) << "\n";
+        break;
+    case 2:
+        cout << "-" << endl;
+        std::cout << "sum = " << min(a, b) << "\n";
+        break;
+    case 3:
+        cout << "-" << endl;
+        std::cout << "sum = " << umg(a, b) << "\n";
+        break;
+    case 5:
+        return 0;
+        break;
+    }
+
+    printf("123");
 }
