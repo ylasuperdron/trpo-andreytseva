52c51c5ff12b09d9144940d2348836ae8efaf61a
antiban.mixxd <antiban.mixxd@outlook.com>
2021-10-14 02:06:00 +0700

    Preventing letters from being entered

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,81 +2,139 @@
 #include <stdio.h>
 #include <string>
 #include <conio.h>
-
+#include <cstring>
 
 using namespace std;
-int a, b, res;
-
-
-int sum(int a, int b)
+double res;
+double del(double a, double b)
 {
-    int res = a;
 
-    for (int i = 1; i <= b; ++i)
+
+    try
     {
-        ++res;
+
+        if (b == 0)
+            throw std::exception("Division by zero\n");
+
+
+        res = a / b;
+        return res;
+
+
+    }
+    catch (std::exception& e)
+    {
+
+        cout << e.what();
+        cout << " Введите второе число снова >> ";
+        cin >> b;
+        return del(a, b);
+
     }
 
+
+
+
+}
+
+double sum(double a, double b)
+{
+    res = a + b;
+
+
+
     return res;
 }
 
-int min(int a, int b)
+double min(double a, double b)
 {
-    int res = a;
+    res = a - b;
 
-    for (int i = 1; i <= b; ++i)
-    {
-        --res;
-    }
 
     return res;
 }
 
-int umg(int a, int b)
+double umg(double a, double b)
 {
-    int res = a * b;
+    res = a * b;
     return res;
 }
 
-int prompt_menu_item()
-{
-    setlocale(LC_CTYPE, "rus");
-    system("cls");
-    cout << " Введите первое число >> ";
-    cin >> a;
-    cout << " Введите второе число >> ";
-    cin >> b;
-    int variant;
-    cout << "Выберите вариант\n" << endl;
-    cout << "1. +\n"
-        << "2. -\n"
-        << "3. *\n"
-        << "5. Выход\n" << endl;
-    cout << ">>> ";
-    cin >> variant;
-    return variant;
-}
 
-int main(int argc, char* argv[])
-{
-    int variant = prompt_menu_item();
 
+void prompt_menu_item(double a, double b, char variant) {
+
+    double rez = 0;
     switch (variant) {
-    case 1: sum(a, b);
-        std::cout << "sum = " << sum(a, b) << "\n";
+    case '+':
+        system("cls");
+        std::cout << a << " + " << b << " = " << sum(a, b) << "\n";
         break;
-    case 2:
-        cout << "-" << endl;
-        std::cout << "sum = " << min(a, b) << "\n";
+    case '-':
+        system("cls");
+        std::cout << a << " - " << b << " = " << min(a, b) << "\n";
         break;
-    case 3:
-        cout << "-" << endl;
-        std::cout << "sum = " << umg(a, b) << "\n";
+    case '*':
+        system("cls");
+        std::cout << a << " * " << b << " = " << umg(a, b) << "\n";
+        cout << b;
         break;
-    case 5:
-        return 0;
+    case '/':
+        system("cls");
+        rez, b = del(a, b);
+        std::cout << a << " / " << b << " = " << rez << "\n";
+
         break;
+
+
+    default:puts("\n Wrong option!");
+
     }
 
-    printf("123");
+
+
+
+}
+
+double prov()
+{
+    if (cin.fail())
+    {
+        cout << "Incorrect input." << endl;
+        exit(0);
+        system("pause");
+    }
+}
+
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    system("cls");
+    double a, b, res;
+    char variant;
+
+
+
+    cout << "Введите первое число        >> ";
+    cin >> a;
+
+    prov();
+
+    cout << "Введите действие(+,-,*,/)   >> ";
+    cin >> variant;
+
+    cout << "Введите второе число        >> ";
+    cin >> b;
+
+    prov();
+
+    prompt_menu_item(a, b, variant);
+    cout << "Для выхода нажмите Esc\n\n";
+    cout << "Для продолжения нажмите Enter";
+    char c;
+    c = _getch();
+
+    if (c == 27) return 0;
+    if (c == 13) main();
+
 }
