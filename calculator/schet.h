8ba6f72c8e7744c922755698ea3fd6367cd0014c
Елизавета Войтюк <elizaveta.voytyuk@yandex.ru>
2021-10-31 23:53:47 +0700

    add structure

---
+++ calculator/schet.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <conio.h>
+#include <cstring>
+
+
+using namespace std;
+double del(double a, double b);
+double sum(double a, double b);
+double minn(double a, double b);
+double umg(double a, double b);
+void prompt_menu_item(double a, double b, char variant);
+double prov();
