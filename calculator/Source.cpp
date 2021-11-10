08fcf6688bc9344e8f9a9b2cbd2c89fa1490efdd
Елизавета Войтюк <elizaveta.voytyuk@yandex.ru>
2021-11-11 01:47:59 +0700

    add correct test

--- calculator/Source.cpp
+++ calculator/Source.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
 #include <conio.h>
-#include "schet.h"
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include "operation.h"
+#include "proverki.h"
+#include "menu.h"
 
-
+double res;
 
 using namespace std;
 
+int main() {
+	setlocale(LC_ALL, "Rus");
+	system("cls");
+	double a, b, res;
+	char variant;
 
-	int main() {
-		setlocale(LC_ALL, "Rus");
-		system("cls");
-		double a, b, res;
-		char variant;
-
-		char A[1000], B[1000];
-
-
-		cout << "Введите первое число: » ";
-		cin >> A;
-
-		a = proverka(A);
+	char A[1000], B[1000];
 
 
-		cout<< "Введите операцию(+,-,*,/) » ";
-		cin >> variant;
+	cout << "Введите первое число: » ";
+	cin >> A;
+
+	a = proverka(A);
 
 
-		cout << "Введите второе число: » ";
-		cin >> B;
-		b = proverka(B);
-
-		prompt_menu_item(a, b, variant);
+	cout << "Введите операцию(+,-,*,/) » ";
+	cin >> variant;
 
 
-		cout << "Для выхода нажмите Esc\n\n";
-		cout<< "Чтобы продолжить нажмите Enter";
-		char c;
-		c = _getch();
+	cout << "Введите второе число: » ";
+	cin >> B;
+	b = proverka(B);
+
+	prompt_menu_item(a, b, variant);
+
+
+	cout << "Для выхода нажмите Esc\n\n";
+	cout << "Чтобы продолжить нажмите Enter";
+	char c;
+	c = _getch();
 
 
 
-		if (c == 27) return 0;
-		if (c != 27) main();
+	if (c == 27) return 0;
+	if (c != 27) main();
 
-	} 
+}
---
+++ calculator/operation.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "cmath"
+
+double sum(double a, double b) {
+	return a + b;
+}
+double minn(double a, double b) {
+	return a - b;
+}
+double umg(double a, double b) {
+	return a * b;
+}
+double del(double a, double b) {
+	return a / b;
+}
